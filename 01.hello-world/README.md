9aca607e8907131c67db19269667e28c0ab6a2d8
ukihsoroy <ko.shen@hotmail.com>
2023-02-28 23:09:46 +0800

    # 完成第一章

---
+++ 01.hello-world/README.md
@@ -0,0 +1,3 @@
+# 微软的go hello world教程
+
+> https://learn.microsoft.com/zh-cn/training/modules/go-variables-functions-packages/
---
+++ 01.hello-world/src/helloworld/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	fmt.Println("Hello World!")
+	fmt.Println(math.Abs(1.2))
+}
